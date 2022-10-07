b1442e8e814482d9750b6f89183c3f01a9363338
lu78abranches <luisabranches.surf@hotmail.com>
2022-10-07 16:34:33 -0300

    Project The Beatles Covers created

---
+++ backend-the-beatles-cover/src/main/java/luisabranches/com/backendthebeatlescover/BackendTheBeatlesCoverApplication.java
@@ -0,0 +1,13 @@
+package luisabranches.com.backendthebeatlescover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTheBeatlesCoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTheBeatlesCoverApplication.class, args);
+	}
+
+}
---
+++ backend-the-beatles-cover/src/test/java/luisabranches/com/backendthebeatlescover/BackendTheBeatlesCoverApplicationTests.java
@@ -0,0 +1,13 @@
+package luisabranches.com.backendthebeatlescover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTheBeatlesCoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
